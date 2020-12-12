523ca6acab8917bc37d106f7af5ac97120d3a3b6
Ronny73 <lasvegaszangmo@gmail.com>
2020-12-12 14:33:01 -0800

    Veritas v-1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ["src/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'hero-pattern': "url('/src/images/heroPattern.jpg')",
+        })
+    },
   },
   variants: {
-    extend: {},
+    theme: {
+      colors: {
+        black: colors.black,
+        white: colors.white,
+        gray: colors.gray,
+        red: colors.red,
+        yellow: colors.yellow,
+        green: colors.green,
+        blue: colors.blue,
+        indigo: colors.indigo,
+        purple: colors.purple,
+      }
+    },
   },
   plugins: [],
 }
